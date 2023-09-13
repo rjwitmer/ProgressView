aee81ce4c75d4e9c0ae376484f5dd48417816fb3
Bob Witmer <bwitmer@golden.net>
2023-09-13 14:55:59 -0400

    Initial Commit

---
+++ ProgressView/ProgressViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewApp.swift
+//  ProgressView
+//
+//  Created by Bob Witmer on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
